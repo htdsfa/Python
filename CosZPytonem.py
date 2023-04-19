02efac5eab6823dfac98e102bad77b9c171ebf27
tak <wojtekfrankiewicz8@gmail.com>
2023-04-19 21:16:47 +0200

    Update CosZPytonem.py
    If i else if

--- CosZPytonem.py
+++ CosZPytonem.py
@@ -4,6 +4,7 @@ obliczonyVAT = (1+VAT/100)
 cenaBrutto = cenaNetto * obliczonyVAT
 print(cenaBrutto)
 
+----------------------------------------
 
 x = 1; y = 2; z = 3;
 
@@ -11,6 +12,7 @@ a,b,c = 1 , "cos" , False
 
 a = b = c = 1
 
+----------------------------------------
 
 abs(-2) #daje 2
 import nazwaBiblioteki #importuje biblioteke
@@ -19,6 +21,7 @@ from nazwaBiblioteki import funkcja #nie trzeba pisac nazwaBiblioteki.funkcja
 from nazwaBiblioteki import * #importuje wszystkie funkcje z biblioteki
 help(funkcja) #pokazuje co robi dana funkcja
 
+----------------------------------------
 
 a = input() #zapisuje dane jako ciąg znaków
 int(a) #zamienia zmienną na liczbe
@@ -30,3 +33,27 @@ b = int(input("Podaj pierwszą liczbę: ")) #zamienia zmienna odrazu na liczbe
 print("Wynki dodawania to:" + str(a+b)) #pokaze: Wynki dodawania to: np 15
 print("Wynki dodawania to:", a+b) #pokaze: Wynki dodawania to: np 15
 print("Wynki dodawania",a,"+",b, "to:", a+b) #pokaze: Wynki dodawania 5 + 10 to: np 15
+
+----------------------------------------
+
+wybor = input("* - mnożenie, / - dzielenie, + - dodawanie, - - odejmowanie")
+
+a = int(input("Pierwsza liczba"))
+b = int(input("Druga liczba"))
+
+if (wybor == "*"):
+    print(a * b)
+elif (wybor == "/"):
+    if(b == 0):
+        print("Nie dziel przez 0")
+    else:
+        print(a / b)
+elif (wybor == "+"):
+    print(a + b)
+elif (wybor == "-"):
+    print(a - b)
+else:
+    print("Nie wybrano nic")
+
+----------------------------------------
+
