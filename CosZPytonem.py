122858377adbe9af721e1d220d14d7224699d266
tak <wojtekfrankiewicz8@gmail.com>
2023-04-19 19:51:12 +0200

    Importowanie bibliotek
    Jak importować i korzystać z bibliotek

--- CosZPytonem.py
+++ CosZPytonem.py
@@ -2,6 +2,7 @@ cenaNetto = 2300
 VAT = 23
 obliczonyVAT = (1+VAT/100)
 cenaBrutto = cenaNetto * obliczonyVAT
+print(cenaBrutto)
 
 
 x = 1; y = 2; z = 3;
@@ -9,3 +10,14 @@ x = 1; y = 2; z = 3;
 a,b,c = 1 , "cos" , False
 
 a = b = c = 1
+
+
+abs(-2) #daje 2
+import nazwaBiblioteki #importuje biblioteke
+nazwaBiblioteki.funkcja #uzycie funckji 
+from nazwaBiblioteki import funkcja #nie trzeba pisac nazwaBiblioteki.funkcja
+from nazwaBiblioteki import * #importuje wszystkie funkcje z biblioteki
+help(funkcja) #pokazuje co robi dana funkcja
+
+
+
