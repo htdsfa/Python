36f2a49d4cff546ad95402b237b4dafd37cffbf0
tak <wojtekfrankiewicz8@gmail.com>
2023-04-21 01:47:57 +0200

    Update Simple_Calculator.py
    Added addition

--- Simple_Calculator.py
+++ Simple_Calculator.py
@@ -1,2 +1,7 @@
 print("+ - addition", "- - subtraction", "* - multiplication", "/ - division", "^ - exponentiation", "# - square root", "% - conversion to percentages")
 choice = input("Pick what You want to do: ")
+if (choice == "+"):
+    firstNum = int(input("Enter first number: "))
+    secondNum = int(input("Enter second number: "))
+    result = firstNum + secondNum
+    print("Your result is",result )
