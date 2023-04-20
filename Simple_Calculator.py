c0fc20ce95e0808621b0b648f858b87a36920b31
tak <wojtekfrankiewicz8@gmail.com>
2023-04-21 01:52:57 +0200

    Update Simple_Calculator.py
    Added multiplication

--- Simple_Calculator.py
+++ Simple_Calculator.py
@@ -9,4 +9,9 @@ if (choice == "-"):
     firstNum = int(input("Enter first number: "))
     secondNum = int(input("Enter second number: "))
     result = firstNum - secondNum
+    print("Your result is", result)if (choice == "*"):
+    firstNum = int(input("Enter first number: "))
+    secondNum = int(input("Enter second number: "))
+    result = firstNum * secondNum
     print("Your result is", result)
+if (choice == "/"):
