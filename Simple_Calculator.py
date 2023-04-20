5e0d734186eef6398480a7488625b3b5a2b0c707
tak <wojtekfrankiewicz8@gmail.com>
2023-04-21 01:47:22 +0200

    Create Simple_Calculator.py
    Created calculator for simple actions

---
+++ Simple_Calculator.py
@@ -0,0 +1,2 @@
+print("+ - addition", "- - subtraction", "* - multiplication", "/ - division", "^ - exponentiation", "# - square root", "% - conversion to percentages")
+choice = input("Pick what You want to do: ")
