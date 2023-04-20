273b4e88ca9c7495f017803611735c14cb27d7a2
tak <wojtekfrankiewicz8@gmail.com>
2023-04-21 01:49:08 +0200

    Update Simple_Calculator.py
    Edited subtraction

--- Simple_Calculator.py
+++ Simple_Calculator.py
@@ -8,5 +8,5 @@ if (choice == "+"):
 if (choice == "-"):
     firstNum = int(input("Enter first number: "))
     secondNum = int(input("Enter second number: "))
-    result = firstNum + secondNum
+    result = firstNum - secondNum
     print("Your result is", result)
