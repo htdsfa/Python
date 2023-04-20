e43d52b2a453b7f3777bbb6178b4ee19b9f26501
tak <wojtekfrankiewicz8@gmail.com>
2023-04-21 01:54:02 +0200

    Update Simple_Calculator.py
    Edited some things

--- Simple_Calculator.py
+++ Simple_Calculator.py
@@ -9,7 +9,8 @@ if (choice == "-"):
     firstNum = int(input("Enter first number: "))
     secondNum = int(input("Enter second number: "))
     result = firstNum - secondNum
-    print("Your result is", result)if (choice == "*"):
+    print("Your result is", result)
+if (choice == "*"):
     firstNum = int(input("Enter first number: "))
     secondNum = int(input("Enter second number: "))
     result = firstNum * secondNum
