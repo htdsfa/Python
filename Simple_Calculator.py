0a61f325c9ca5c79f0947df32ef7434fba5d6854
tak <wojtekfrankiewicz8@gmail.com>
2023-04-21 02:25:00 +0200

    Update Simple_Calculator.py

--- Simple_Calculator.py
+++ Simple_Calculator.py
@@ -1,35 +1,44 @@
-print("+ - addition", "- - subtraction", "* - multiplication", "/ - division", "^ - exponentiation", "# - square root", "% - conversion to percentages")
-choice = input("Pick what You want to do: ")
-if (choice == "+"):
-    firstNum = int(input("Enter first number: "))
-    secondNum = int(input("Enter second number: "))
-    result = firstNum + secondNum
-    print("Your result is",result )
-if (choice == "-"):
-    firstNum = int(input("Enter first number: "))
-    secondNum = int(input("Enter second number: "))
-    result = firstNum - secondNum
-    print("Your result is", result)
-if (choice == "*"):
-    firstNum = int(input("Enter first number: "))
-    secondNum = int(input("Enter second number: "))
-    result = firstNum * secondNum
-    print("Your result is", result)
-if (choice == "/"):
-    firstNum = int(input("Enter first number: "))
-    secondNum = int(input("Enter second number: "))
-    if (secondNum == 0):
-        print("Do not divide by 0!!!")
-    else:
-        result = firstNum / secondNum
+while(True):
+    print("+ - addition", "- - subtraction", "* - multiplication", "/ - division", "^ - exponentiation", "# - square root", "% - conversion to percentages", "| - end")
+    choice = input("Pick what You want to do: ")
+    if (choice == "+"):
+        firstNum = int(input("Enter first number: "))
+        secondNum = int(input("Enter second number: "))
+        result = firstNum + secondNum
+        print("Your result is",result )
+    if (choice == "-"):
+        firstNum = int(input("Enter first number: "))
+        secondNum = int(input("Enter second number: "))
+        result = firstNum - secondNum
         print("Your result is", result)
-if (choice == "^"):
-    firstNum = int(input("Enter a power number: "))
-    secondNum = int(input("Enter power: "))
-    result = firstNum ** secondNum
-    print("Your result is", result)
-if (choice == "#"):
-    import math
-    Num = int(input("Enter number: "))
-    result = math.sqrt(Num)
-    print("Your result is", result)
+    if (choice == "*"):
+        firstNum = int(input("Enter first number: "))
+        secondNum = int(input("Enter second number: "))
+        result = firstNum * secondNum
+        print("Your result is", result)
+    if (choice == "/"):
+        firstNum = int(input("Enter first number: "))
+        secondNum = int(input("Enter second number: "))
+        if (secondNum == 0):
+            print("Do not divide by 0!!!")
+        else:
+            result = firstNum / secondNum
+            print("Your result is", result)
+    if (choice == "^"):
+        firstNum = int(input("Enter a power number: "))
+        secondNum = int(input("Enter power: "))
+        result = firstNum ** secondNum
+        print("Your result is", result)
+    if (choice == "#"):
+        import math
+        Num = int(input("Enter number: "))
+        result = math.sqrt(Num)
+        print("Your result is", result)
+    if (choice == "%"):
+        firstNum = int(input("Enter number: "))
+        secondNum = int(input("Enter percentages: "))
+        print("Your result is", result)
+    if (choice == "|"):
+        break
+    else:
+        print("You did not pick right option!!!")
