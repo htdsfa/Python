b357c9b6d420ca64db9f6ea9a10879b06d9bff45
tak <wojtekfrankiewicz8@gmail.com>
2023-04-24 20:21:12 +0200

    Create Funkcja.py

---
+++ Import/Funkcja.py
@@ -0,0 +1,16 @@
+import math
+
+def poleKwadratu(a):
+    return a*a
+
+def poleProstokąta(a,b):
+    return a*b
+
+def poleKola(r):
+    return math.pi*r**2
+
+def poleTrojkata(a,h):
+    return 0.5*a*h
+
+def poleTrapezu(a,b,h):
+    return (a+b)/2*h
