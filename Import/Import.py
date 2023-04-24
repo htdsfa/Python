d40470ad5b2af5d32546c0696dd1bbcce63bbcff
tak <wojtekfrankiewicz8@gmail.com>
2023-04-24 20:26:58 +0200

    Update Import.py

--- Import/Import.py
+++ Import/Import.py
@@ -1,8 +1,8 @@
 import Funkcja
 
 
-from enum import Enum
-Menu = Enum('Menu', 'kwadrat prostokat kolo trojkat trapez')
+from enum import IntEnum
+Menu = IntEnum('Menu', 'kwadrat prostokat kolo trojkat trapez')
 
 wybor = int(input("""Jakie pole chcesz obliczyc:
 1 - kwadrat
@@ -13,21 +13,21 @@ wybor = int(input("""Jakie pole chcesz obliczyc:
 
 """))
 
-if (wybor == Menu.kwadrat.value):
+if (wybor == Menu.kwadrat):
     a = int(input("Podaj dlugosc krawedzi: "))
     print("Pole wynosi: ", Funkcja.poleKwadratu(a))
-elif(wybor == Menu.prostokat.value):
+elif(wybor == Menu.prostokat):
     a = int(input("Podaj dlugosc pierwszej krawedzi: "))
     b = int(input("Podaj dlugosc drugiej krawedzi: "))
     print("Pole wynosi: ", Funkcja.poleProstokąta(a,b))
-elif(wybor == Menu.kolo.value):
+elif(wybor == Menu.kolo):
     a = int(input("Podaj promien: "))
     print("Pole wynosi: ", Funkcja.poleKola(a))
-elif(wybor == Menu.trojkat.value):
+elif(wybor == Menu.trojkat):
     a = int(input("Podaj dlugosc podstawy: "))
     h = int(input("Podaj dlugosc wysokosci: "))
     print("Pole wynosi: ", Funkcja.poleTrojkata(a,h))
-elif(wybor == Menu.trapez.value):
+elif(wybor == Menu.trapez):
     a = int(input("Podaj dlugosc pierwszej krawedzi: "))
     b = int(input("Podaj dlugosc drugiej krawedzi: "))
     h = int(input("Podaj dlugosc wysokosci: "))
